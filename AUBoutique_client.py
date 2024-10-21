321eb3d1e0b5cc8bc3fc30339dbe6f7ecec45237
Nohra2005 <95567892+Nohra2005@users.noreply.github.com>
2024-10-21 15:36:10 +0300

    Update AUBoutique_client.py

--- AUBoutique_client.py
+++ AUBoutique_client.py
@@ -1,32 +1,29 @@
 from socket import*
 import json
-from protocol import register_client
+from protocol import register_client, login_client
 
+# Client setup
 client = socket(AF_INET, SOCK_STREAM)
-server_host = 'localhost'  
-server_port = 8888  
-try:
-    client.connect((server_host, server_port))
-    message = client.recv(1024).decode('utf-8')
-    print(message)  
+client.connect(('localhost', 8888))
 
-    #Client choses if he wants to register or log in
-    choice = input("Enter your choice (1 or 2): ")
-    client.send(choice.encode('utf-8'))
+while True:
+    print("1. Register")
+    print("2. Login")
+    choice = input("Enter your choice: ")
 
-
-    #Register
     if choice == "1":
         registration_data = register_client()
-         #Convert to json and send registration information to server
-        registration_message = json.dumps(registration_data).encode('utf-8')
-        client.send(registration_message)
-        registration_status = client.recv(1024).decode('utf-8')
-        print(registration_status)
-
-except ConnectionRefusedError:
-    print("Failed to connect to the server.")
-
-finally:
-    # Close the connection
-    client.close()
+        client.send(json.dumps(registration_data).encode('utf-8'))
+        response = client.recv(1024).decode('utf-8')
+        print(response)
+    elif choice == "2":
+        login_data = login_client()
+        client.send(json.dumps(login_data).encode('utf-8'))
+        response = client.recv(1024).decode('utf-8')
+        print(response)
+        
+        if response == "Login successful":
+            print("Proceeding to next task...")
+            break
+    else:
+        print("Invalid choice. Try again.")
